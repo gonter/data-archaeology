3912c06102d71c0acfb423e16f5f2516055dc980
Gerhard Gonter <ggonter@gmail.com>
2012-01-17 08:57:47 +0100

    restructure source data into a perl datastructure

--- excavations/site0001/ext001.pl
+++ excavations/site0001/ext001.pl
@@ -35,10 +35,7 @@ while (defined ($arg= shift (@ARGV)))
   push (@JOBS, $arg);
 }
 
-while (defined ($arg= shift (@JOBS)))
-{
   &main_function ($arg);
-}
 
 exit (0);
 
@@ -60,10 +57,115 @@ sub main_function
   my $fnm= shift;
   print "main_function: $fnm\n";
 
-  my $db= new DBF::Unknown1;
-  $db->parse ($fnm, 1);
-  my $cnt= @{$db->{'records'}};
-  print "db: recs=[$cnt] ", Dumper ($db);
+  my %all= ();
+
+  my $db_pers= new DBF::Unknown1;
+  $db_pers->parse ('PERS.DAT', 1);
+  my $cnt= @{$db_pers->{'records'}};
+  # print "db_pers: recs=[$cnt] ", Dumper ($db_pers);
+  my $st_pers= &analyze_records ($db_pers, \%all, 'pers');
+
+  print "stats pers: ", Dumper ($st_pers);
+
+  my $cnt_pers= scalar keys %all;
+  print __LINE__, " cnt=[$cnt_pers]\n";
+
+  my $db_tit= new DBF::Unknown1;
+  $db_tit->parse ('TIT.DAT', 1);
+  my $cnt= @{$db_tit->{'records'}};
+  # print "db_tit: recs=[$cnt] ", Dumper ($db_tit);
+  my $st_tit= &analyze_records ($db_tit, \%all, 'title');
+
+  print "stats tit: ", Dumper ($st_tit);
+
+  my $cnt_pers= scalar keys %all;
+  print __LINE__, " cnt=[$cnt_pers]\n";
+
+  print "all: ", Dumper (\%all);
+}
+
+# transform records into a more structured format
+sub analyze_records
+{
+  my $db= shift;
+  my $out= shift; # collection of all records, ordered by Ident.Nr.
+  my $part= shift;
+
+  my %stats= ();
+  foreach my $rec (@{$db->{'records'}})
+  {
+    # print "rec: ", Dumper ($rec);
+    my %out_record= ();
+    my $ident_nr= undef;
+
+    foreach my $field (@{$rec->{'fields'}})
+    {
+      # correct two broken fields
+         if ($field =~ m#^28  (.+)#) { $field= '280 ' . $1 }
+      elsif ($field =~ m#^2457(.+)#) { $field= '245 ' . $1 }
+
+      if ($field =~ m#^(\d\d\d) (.+)#)
+      {
+        my ($key, $val)= ($1, $2);
+	push (@{$out_record{$key}}, $val);
+	$val=~ s# *$##;
+	$stats{$key}++;
+
+	if ($key == "000")
+	{
+          # ident_nr field looks like this '000 555.001'
+	  $val =~ s# ##g; # apparently, there are entries with blanks in it...
+
+	  if ($val =~ /^(\d+)$/ && $part eq 'pers')
+	  {
+	    $ident_nr= $1+0;
+	    # print "ident_nr: #1 [$val] => [$ident_nr]\n";
+            push (@{$out->{$ident_nr}->{'pers'}}, \%out_record);
+	  }
+	  elsif ($val =~ /^(\d+)\.(\d+)$/ && $part eq 'title')
+	  {
+	    my ($k1, $k2)= ($1, $2);
+	    $k1 += 0;
+	    $k2 += 0;
+	    $ident_nr= $k1+0;
+
+	    # print "ident_nr: #2 [$val] => [$k1], [$k2] => [$ident_nr]\n";
+            push (@{$out->{$ident_nr}->{'title'}->{$k2}}, \%out_record);
+	  }
+	  else
+	  {
+            print "ATTN: unknown ident_nr format! [$field]\n";
+            print "rec: ", Dumper ($rec);
+	  }
+	}
+      }
+      else
+      {
+        print "ATTN: unknown field format! [$field]\n";
+        print "rec: ", Dumper ($rec);
+      }
+    }
+
+    unless (defined ($ident_nr))
+    {
+      print "ATTN: no ident nr found in record: ", Dumper ($rec);
+      next;
+    }
+
+  }
+
+  \%stats;
+}
+
+sub analyze_pers
+{
+  my $db_pers= shift;
+
+  # order persons by Ident.Nr.
+  foreach my $rec (@{$db_pers->{'records'}})
+  {
+  }
+
 }
 
 # ----------------------------------------------------------------------------
